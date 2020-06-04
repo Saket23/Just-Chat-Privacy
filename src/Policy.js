78b510aad6a51a3b9a4996207a1d2f668fe77b49
saket sinha <saket.sinha@vedantu.com>
2020-06-04 20:34:49 +0530

    just chat privacy policy

---
+++ src/Policy.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+const Policy = () => {
+  return (
+    <div className="policy">
+      <h2>Just Chat PRIVACY POLICY</h2>
+
+      <p>Last Updated: 4th June 2020</p>
+
+      <p>
+        <b>[Just Chat]</b>, we respects the privacy of our users. This Privacy
+        Policy explains how we collect, use, disclose, and safeguard your
+        information when you visit our mobile application (the “Application”).
+        Please read this Privacy Policy carefully. IF YOU DO NOT AGREE WITH THE
+        TERMS OF THIS PRIVACY POLICY, PLEASE DO NOT ACCESS THE APPLICATION.
+      </p>
+
+      <p>
+        We reserve the right to make changes to this Privacy Policy at any time
+        and for any reason. We will alert you about any changes by updating the
+        “Last updated” date of this Privacy Policy. You are encouraged to
+        periodically review this Privacy Policy to stay informed of updates. You
+        will be deemed to have been made aware of, will be subject to, and will
+        be deemed to have accepted the changes in any revised Privacy Policy by
+        your continued use of the Application after the date such revised
+        Privacy Policy is posted.
+      </p>
+
+      <p>
+        This Privacy Policy does not apply to the third-party online/mobile
+        store from which you install the Application or make payments, including
+        any in-game virtual items, which may also collect and use data about
+        you. We are not responsible for any of the data collected by any such
+        third party.
+      </p>
+
+      <h4>COLLECTION OF YOUR INFORMATION</h4>
+      <p>
+        We may collect information about you in a variety of ways. The
+        information we may collect via the Application depends on the content
+        and materials you use, and includes:
+      </p>
+
+      <h5>Read Call log:</h5>
+      <p>
+        We will read you call log and display it on our app so that you can
+        easily tap on the last dialled number and visit whatsapp.
+      </p>
+
+      <h4>DISCLOSURE OF YOUR INFORMATION</h4>
+      <p>
+        Under no situation your call log details are going out of your phone.
+        Your information is save and secure in your phone.
+      </p>
+
+      <p>
+        If you have questions or comments about this Privacy Policy, please
+        contact us at:
+      </p>
+      <div>
+        <div>Just Chat</div>
+        <div>Deepthi Nagar, JP nagar 8th Phase</div>
+        <div>Bangalore, Karnataka, India</div>
+        <div>+91-9663466135</div>
+        <div>saketsinha23@gmail.com</div>
+      </div>
+    </div>
+  );
+};
+
+export default Policy;
